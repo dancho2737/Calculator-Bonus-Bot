6de66cc042c38f923abf334acd9e00d246ee65a6
dancho2737 <churocov9702@gmail.com>
2025-07-27 15:34:30 +0600

    Update state.py

--- kyrgyzbot/state.py
+++ kyrgyzbot/state.py
@@ -1,13 +1,3 @@
-
-# state.py
-
-# Выбор режима: с кыргызского или на кыргызский
-MODE_SELECTION = 1
-
-# Режим: с кыргызского на число
-GET_KG_INPUT = 2
-CHECK_KG_INPUT = 3
-
-# Режим: число -> кыргызское слово
-GET_NUMBER_INPUT = 4
-CHECK_NUMBER_INPUT = 5
+CHOOSING_MODE = 1
+WORD_QUIZ = 2
+NUMBER_QUIZ = 3
